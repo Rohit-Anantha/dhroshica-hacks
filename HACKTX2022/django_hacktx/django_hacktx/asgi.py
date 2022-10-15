6257f677c600e7ace834c42b96ca8d47b4621137
Rohit Anantha <anantha.rohit.11@gmail.com>
2022-10-15 13:49:58 -0500

    set up directory

--- HACKTX2022/django_hacktx/django_hacktx/asgi.py
+++ HACKTX2022/django_hacktx/django_hacktx/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for HACKTX2022 project.
+ASGI config for django_hacktx project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HACKTX2022.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_hacktx.settings')
 
 application = get_asgi_application()
